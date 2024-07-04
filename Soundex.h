53c39e59fbf6c81c8b74ceea454f5126873d45d5
Preethiravi04 <preethiravi0499@gmail.com>
2024-07-04 13:21:07 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -14,25 +14,33 @@ char getSoundexCode(char c) {
     return (index < 26) ? soundexCodes[index] : '0';
 }
 
+void appendSoundexChar(char *soundex, int *sIndex, char code) {
+    if (*sIndex < 4) {
+        soundex[(*sIndex)++] = code;
+    }
+}
+
+void fillRemainingWithZeroes(char *soundex, int *sIndex) {
+    while (*sIndex < 4) {
+        soundex[(*sIndex)++] = '0';
+    }
+}
+
 void generateSoundex(const char *name, char *soundex) {
-    soundex[0] = toupper(name[0]);  
-    char previousCode = getSoundexCode(name[0]);  
-    int sIndex = 1;  // Initialize index for soundex array
-    
+    soundex[0] = toupper(name[0]);
+    char previousCode = getSoundexCode(name[0]);
+    int sIndex = 1;
+
     for (int i = 1; name[i] != '\0' && sIndex < 4; i++) {
-        char code = getSoundexCode(name[i]);  
-        
+        char code = getSoundexCode(name[i]);
         if (code != '0' && code != previousCode) {
-            soundex[sIndex++] = code;  
-            previousCode = code; 
+            appendSoundexChar(soundex, &sIndex, code);
+            previousCode = code;
         }
     }
-    
-    while (sIndex < 4) {
-        soundex[sIndex++] = '0';  // Fill remaining positions with '0'
-    }
-    
-    soundex[4] = '\0';  // Ensure soundex string is null-terminated
+
+    fillRemainingWithZeroes(soundex, &sIndex);
+    soundex[4] = '\0';
 }
 
 #endif
